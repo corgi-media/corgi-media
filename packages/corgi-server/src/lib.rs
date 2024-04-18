2e425e260d5688f4f0707feb43673766ded19788
Zue <zuegoo@gmail.com>
2024-04-18 21:34:41 +0800

    chore: add http:// prefix to startup log

--- packages/corgi-server/src/lib.rs
+++ packages/corgi-server/src/lib.rs
@@ -9,7 +9,7 @@ pub async fn start(addr: SocketAddr) {
 
     let app = Router::new().merge(routes::docs_routers());
 
-    tracing::info!("Corgi server running on {}", addr);
+    tracing::info!("Corgi server running on http://{}", addr);
 
     axum::serve(listener, app.into_make_service())
         .await
