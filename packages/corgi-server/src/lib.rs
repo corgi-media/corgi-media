334bbb7efe40277dddc52bd61a36b80d91fb0c00
Zue <zuegoo@gmail.com>
2024-04-19 21:22:14 +0800

    feat: axum trace layer

--- packages/corgi-server/src/lib.rs
+++ packages/corgi-server/src/lib.rs
@@ -3,11 +3,14 @@ mod routes;
 use std::net::SocketAddr;
 
 use axum::Router;
+use tower_http::trace::TraceLayer;
 
 pub async fn start(addr: SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
     let listener = tokio::net::TcpListener::bind(addr).await?;
 
-    let app = Router::new().merge(routes::ApiDocsRouter::route());
+    let app = Router::new()
+        .merge(routes::ApiDocsRouter::route())
+        .layer(TraceLayer::new_for_http());
 
     tracing::info!("Corgi server running on http://{}", addr);
 
