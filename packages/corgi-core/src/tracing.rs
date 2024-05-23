09b1fbbbe7fb253ce18a3406fc479b3c9c8232e6
Zue <zuegoo@gmail.com>
2024-05-23 19:44:35 +0800

    Add default EnvFilter in core::tracing::with_default fn

--- packages/corgi-core/src/tracing.rs
+++ packages/corgi-core/src/tracing.rs
@@ -1,11 +1,13 @@
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 pub use tracing::*;
 
 pub fn with_default<T>(f: impl FnOnce() -> T) -> T {
-    let subscriber = tracing_subscriber::fmt().finish();
+    let subscriber = tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env().add_directive(Level::TRACE.into()))
+        .finish();
 
-    let _guard = tracing::subscriber::set_default(subscriber);
+    let _guard = subscriber::set_default(subscriber);
 
     f()
 }
