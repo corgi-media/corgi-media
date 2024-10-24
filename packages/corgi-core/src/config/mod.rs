f8d18c803a78f1399b936034f481f8621695556a
Zue <zuegoo@gmail.com>
2024-10-24 21:02:19 +0800

    feat: impl config status & make AppConfig serializable

--- packages/corgi-core/src/config/mod.rs
+++ packages/corgi-core/src/config/mod.rs
@@ -1,9 +1,17 @@
 pub mod database;
 pub mod server;
 
+use serde::{Deserialize, Serialize};
+
 pub use database::DatabaseConfig;
 pub use server::ServerConfig;
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct AppConfigStatus {
+    pub database: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct AppConfig {
     pub server: ServerConfig,
     pub database: Option<DatabaseConfig>,
@@ -16,5 +24,11 @@ impl AppConfig {
         let database = DatabaseConfig::read(&server.config_database_path(), &server.data_path);
 
         Self { server, database }
+    }
+
+    pub fn status(&self) -> AppConfigStatus {
+        let database = self.database.is_some();
+
+        AppConfigStatus { database }
     }
 }
