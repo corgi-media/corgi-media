d162b25d10fa7161ef23b12e23ab6bec8c082e34
Zue <zuegoo@gmail.com>
2024-11-17 15:38:15 +0800

    chore: export orm

--- packages/corgi-database/src/lib.rs
+++ packages/corgi-database/src/lib.rs
@@ -1,28 +1,28 @@
 pub mod entities;
 pub mod migrations;
 
-pub use sea_orm::{ConnectOptions, Database, DatabaseConnection, DbErr};
+pub use sea_orm as orm;
 
 use migrations::{Migrator, MigratorTrait};
 
 pub struct DatabaseClient {
-    pub connection: DatabaseConnection,
+    pub connection: orm::DatabaseConnection,
 }
 
 impl DatabaseClient {
-    pub async fn connect(url: &str) -> Result<DatabaseClient, DbErr> {
-        let options = ConnectOptions::new(url);
+    pub async fn connect(url: &str) -> Result<DatabaseClient, orm::DbErr> {
+        let options = orm::ConnectOptions::new(url);
 
-        let connection = Database::connect(options).await?;
+        let connection = orm::Database::connect(options).await?;
 
         Ok(Self { connection })
     }
 
-    pub async fn migration_up(&self) -> Result<(), DbErr> {
+    pub async fn migration_up(&self) -> Result<(), orm::DbErr> {
         Migrator::up(&self.connection, None).await
     }
 
-    pub async fn migration_down(&self) -> Result<(), DbErr> {
+    pub async fn migration_down(&self) -> Result<(), orm::DbErr> {
         Migrator::down(&self.connection, None).await
     }
 }
