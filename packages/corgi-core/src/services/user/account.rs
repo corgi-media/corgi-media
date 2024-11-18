c3c0b17df0852bf64441142efbeb2509a332e500
Zue <zuegoo@gmail.com>
2024-11-18 22:19:28 +0800

    feat: administrator = true if user table is empty

--- packages/corgi-core/src/services/user/account.rs
+++ packages/corgi-core/src/services/user/account.rs
@@ -13,12 +13,15 @@ pub async fn account_create(
     username: String,
     password: String,
 ) -> Result<UserSchema, crate::error::Error> {
+    let user_empty = super::is_user_table_empty(db).await?;
     let password_hash = crate::utils::password::hash(password).await?;
+
     let user = user::ActiveModel {
         id: Set(Uuid::now_v7()),
         name: Set(name),
         username: Set(username),
         password: Set(password_hash),
+        administrator: Set(user_empty),
         ..Default::default()
     }
     .insert(db)
--- packages/corgi-core/src/services/user/mod.rs
+++ packages/corgi-core/src/services/user/mod.rs
@@ -1,3 +1,12 @@
 mod account;
 
+use corgi_database::{
+    entities::user,
+    orm::{DatabaseConnection, DbErr, EntityTrait, QuerySelect},
+};
+
 pub use account::*;
+
+pub async fn is_user_table_empty(db: &DatabaseConnection) -> Result<bool, DbErr> {
+    Ok(user::Entity::find().limit(1).all(db).await?.is_empty())
+}
