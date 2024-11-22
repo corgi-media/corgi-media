c30cb9de48474d26a062e524b173f47744141f03
Zue <zuegoo@gmail.com>
2024-11-22 21:53:22 +0800

    feat: openapi SecurityAddon JWT

--- packages/corgi-server/src/openapi.rs
+++ packages/corgi-server/src/openapi.rs
@@ -1,8 +1,27 @@
-use utoipa::OpenApi;
+use utoipa::{
+    openapi::security::{Http, HttpAuthScheme, SecurityScheme},
+    Modify, OpenApi,
+};
 
 #[derive(OpenApi)]
-#[openapi(info(title = "Corgi API",))]
+#[openapi(
+    info(title = "Corgi API"),
+    modifiers(&SecurityAddon),
+)]
 pub struct Docs;
+
+struct SecurityAddon;
+
+impl Modify for SecurityAddon {
+    fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+        if let Some(components) = openapi.components.as_mut() {
+            components.add_security_scheme(
+                "JWT",
+                SecurityScheme::Http(Http::new(HttpAuthScheme::Bearer)),
+            )
+        }
+    }
+}
 
 pub struct Tags;
 
