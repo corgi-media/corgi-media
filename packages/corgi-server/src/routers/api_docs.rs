4b3dedb658ded73dd1e5741dbadbc1963ad9faca
Zue <zuegoo@gmail.com>
2025-01-07 20:00:04 +0800

    Set SwaggerUi configuration persist_authorization to true

--- packages/corgi-server/src/routers/api_docs.rs
+++ packages/corgi-server/src/routers/api_docs.rs
@@ -6,7 +6,9 @@ use super::Paths;
 use crate::state::AppState;
 
 pub fn route(api: OpenApi) -> Router<AppState> {
-    let config = Config::new([Url::new("Corgi API", Paths::OPENAPI_JSON)]);
+    let config =
+        Config::new([Url::new("Corgi API", Paths::OPENAPI_JSON)]).persist_authorization(true);
+
     let swagger = SwaggerUi::new(Paths::SWAGGER_UI)
         .url(Paths::OPENAPI_JSON, api)
         .config(config);
